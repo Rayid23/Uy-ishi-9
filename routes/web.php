8bed68df0a2d434edc38f12ded03349a96b5e135
Madiyor <madiyorshodiev333@gmail.com>
2024-10-22 02:32:50 +0500

    22.10.2024 2:32

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,27 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 //Dashboard
-Route::get('/', function () {
-    return view('dashboard.adminDashboard');
-});
-Route::get('/dashboard2', function () {
-    return view('dashboard.adminDashboard2');
-});
-Route::get('/dashboard3', function () {
-    return view('dashboard.adminDashboard3');
-});
-
-Route::get('/iframe', function () {
-    return view('iframe.iframe');
-});
+Route::get('/', [AdminController::class, 'adminDashboard1']);
+Route::get('/dashboard2', [AdminController::class, 'adminDashboard2']);
+Route::get('/dashboard3', [AdminController::class, 'adminDashboard3']);
+Route::get('/iframe', [AdminController::class, 'iframePage']);
 
 // Pages папка
-Route::get('/widgets', function () {
-    return view('pages.widgets');
-});
-Route::get('/kanban', function () {
-    return view('pages.kanban');
-});
-Route::get('/gallery', function () {
-    return view('pages.gallery');
-});
-Route::get('/calendar', function () {
-    return view('pages.calendar');
-});
+Route::get('/widgets', [AdminController::class, 'WidgetsPage']);
+Route::get('/kanban', [AdminController::class, 'KanbanPage']);
+Route::get('/gallery', [AdminController::class, 'GelleryPage']);
+Route::get('/calendar', [AdminController::class, 'CalendarPage']);
 
+//Панель пользователя
+Route::get('/user-panel', [UserController::class, 'index']);
+Route::get('/servis-details', [UserController::class, 'servisDetails']);
+Route::get('/blog', [UserController::class, 'blog']);
+Route::get('/blog-details', [UserController::class, 'blogDetails']);
+Route::get('/portfolio-details', [UserController::class, 'portfolioDetails']);
 // laravel
-Route::get('/laravel', function () {
-    return view('welcome');
-});
+Route::get('/laravel', [AdminController::class, 'laravel']);
+
